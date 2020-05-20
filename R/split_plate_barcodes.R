1831e8f30bd3f552ebddd6dbfbd0679a06b4f475
Siddiqui <sidd10@dskmanageits-MacBook-Pro.local>
2020-05-19 20:22:14 -0400

    Added the split plate barcodes function.  To be tested

---
+++ R/split_plate_barcodes.R
@@ -0,0 +1,51 @@
+#' Split by plate barcodes
+#'
+#' Split fastq files by the plate barcodes provided
+#'
+#' @param pair1_filepath file path of the first paired end file
+#' @param pair2_filepath file path of the second paired end file
+#' @param nreads number of reads to iterate by
+#' @output_prefix The output prefix for the output files
+#'
+#' @export
+#'
+
+split_plate_barcodes <- function(pair1_filepath, pair2_filepath,
+                                 forward_barcode, nreads = 100,
+                                 output_prefix = "outputfile"){
+
+forward_barcoded_files1 = c()
+forward_barcoded_files2 = c()
+for(i in 1:length(forward_barcode)){
+    forward_barcoded_files1[i] = paste(output_prefix, "_split_", forward_barcode[i], "_1.fq", sep = "")
+    forward_barcoded_files2[i] = paste(output_prefix, "_split_", forward_barcode[i], "_2.fq", sep = "")
+}
+
+
+f1 <- ShortRead::FastqStreamer(pair1_filepath, nreads)
+f2 <- ShortRead::FastqStreamer(pair2_filepath, nreads)
+fq1 <- ShortRead::yield(f1)
+fq2 <- ShortRead::yield(f2)
+
+while(length(fq1)){
+
+
+  for(i in 1:length(forward_barcode)){
+
+    selected_reads = grepl(pattern = forward_barcode[i], ShortRead::sread(fq1))
+    fqsub1 <- fq1[selected_reads]
+    fqsub2 <- fq2[selected_reads]
+
+    file.name1 = forward_barcoded_files1[i]
+    file.name2 = forward_barcoded_files2[i]
+    ShortRead::writeFastq(fqsub1,file.name1,mode="a",compress=FALSE)
+    ShortRead::writeFastq(fqsub2,file.name2,mode="a",compress=FALSE)
+  }
+  fq1 <- ShortRead::yield(f1)
+  fq2 <- ShortRead::yield(f2)
+
+
+}
+
+}
+
